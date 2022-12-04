fd38e38a139260fb476256e82df2e7700c119a20
Ricky Sweat <Antimarvin@gmail.com>
2022-12-04 10:33:06 -0500

    trucks can diferentiate between mine containers and storage containers.

--- src/creeps/Truck.js
+++ src/creeps/Truck.js
@@ -14,6 +14,7 @@ var roleTruck = {
             let availableContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: s => s.structureType === STRUCTURE_CONTAINER
                     && s.store.energy > 0
+                    && s.room.memory.containers[s.id].mine === true
             })
             let droppedResources = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES,  {
                 filter: r => r.amount >= creep.store.getFreeCapacity()
@@ -31,15 +32,30 @@ var roleTruck = {
         }
         else if (creep.memory.working) {
             //creep.say('Vroom');
-            let structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            let primaryStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: s => (s.structureType === STRUCTURE_SPAWN
                            || s.structureType === STRUCTURE_EXTENSION
                            || s.structureType === STRUCTURE_TOWER)
                            && s.energy < s.energyCapacity
             });
+            let secondaryStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s => (s.structureType === STRUCTURE_CONTAINER)
+                          && s.store.energy < s.store.getCapacity()
+                          && s.room.memory.containers[s.id].mine === false
+            });
 
-            if (creep.transfer(structure,RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(structure);
+            if(primaryStructure) {
+                if (creep.transfer(primaryStructure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(primaryStructure)
+                }
+            }
+            else if (secondaryStructure) {
+                if (creep.transfer(secondaryStructure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(secondaryStructure);
+                }
+            }
+            else {
+                creep.say("Stalled")
             }
         }
     }
