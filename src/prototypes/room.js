ba9fb9a2a9623a4da77079056efb2f81799b3b1d
Ricky Sweat <Antimarvin@gmail.com>
2022-12-04 10:23:14 -0500

    marked containers as mines or storage

--- src/prototypes/room.js
+++ src/prototypes/room.js
@@ -1,6 +1,36 @@
-//Initialize rooms with data
-Room.prototype.init = function init(){
-    console.log("Running init for ." + this.name)
+
+Room.prototype.updateContainerProps = function (debug_status){
+    console.log("Running Container Props for ." + this.name)
+    if(!this.memory.containers || debug_status) {
+        this.memory.containers = {}
+    }
+
+    let containers = this.find(FIND_STRUCTURES, {
+        filter: s => (s.structureType === STRUCTURE_CONTAINER)
+    })
+
+    let sources = this.find(FIND_SOURCES)
+
+    for(let c of containers){
+        if(!this.memory.containers[c.id]){
+
+            this.memory.containers[c.id] = {}
+            let isSourceContainer = false
+
+            for(let s of sources){
+                if(c.pos.isNearTo(s)){
+                    isSourceContainer = true
+                    break
+                }
+            }
+            this.memory.containers[c.id].mine = isSourceContainer;
+        }
+    }
+}
+
+//Initialize hrPlan with base role quantities
+Room.prototype.createHRPlan = function (){
+    console.log("Running HR plan for ." + this.name)
     this.memory.hrPlan = {
         harvester: {
             role: 'harvester',
@@ -27,13 +57,18 @@ Room.prototype.init = function init(){
             minQty: 1
         }
     }
-
 }
 
 /** @param {Boolean} debug_status **/
 Room.prototype.update = function update(debug_status) {
     //if this room has no memory then initialize the room
     if (!this.memory.hrPlan || debug_status) {
-        this.init();
+        this.createHRPlan();
+    }
+    if(!this.memory.containers || debug_status){
+        this.updateContainerProps();
+    }
+    if(!this.memory.containers || debug_status){
+        this.updateContainerProps(debug_status);
     }
 }
