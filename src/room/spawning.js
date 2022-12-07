b8f61f008bae45d5a57ad60e9173f0ae1dd904bb
Richard L. Sweat Jr <Antimarvin@gmail.com>
2022-12-07 15:09:30 -0500

    added a bunch of comments and added harvester restart logic

--- src/room/spawning.js
+++ src/room/spawning.js
@@ -3,24 +3,29 @@
 
 function spawnCreeps(room) {
     let spawns = room.find(FIND_MY_SPAWNS)
+
+    //loop through all spawns
     for (let s of spawns) {
         let creepsInRoom = room.find(FIND_MY_CREEPS)
         let hrPlan = room.memory.hrPlan
 
-        let spawningPriority = ['harvester', 'builder', 'upgrader']
-
-
         for(let r in hrPlan) {
             let numInRole = _.sum(creepsInRoom, c => c.memory.role === hrPlan[r].role)
 
+            //if harvesters are 0 spawn one with available energy rather than max
+            if(_.sum(creepsInRoom, c => c.memory.role === 'harvester') === 0 ){
+                s.createHarvester(hrPlan[r].role, s.room.energyAvailable)
+            }
+
+            //loop through roles checking current quantity against max qty and spawn if needed.
             if(numInRole < hrPlan[r].minQty){
                 if(hrPlan[r].role === 'harvester'){
                     //console.log ("Attempting Harvester spawn of " + hrPlan[r].role + " with " + s.room.energyCapacityAvailable)
-                    s.createHarvester(hrPlan[r].role, s.room.energyAvailable)
+                    s.createHarvester(hrPlan[r].role, s.room.energyCapacityAvailable)
                 }
                 else if(hrPlan[r].role === 'truck'){
                     //console.log ("Attempting Harvester spawn of " + hrPlan[r].role + " with " + s.room.energyCapacityAvailable)
-                    s.createTruck(hrPlan[r].role, s.room.energyAvailable)
+                    s.createTruck(hrPlan[r].role, s.room.energyCapacityAvailable)
                 }
                 else {
                     //console.log ("Attempting Scaling spawn of " + hrPlan[r].role + " with " + s.room.energyCapacityAvailable)
