63400457ab7e0a46597479d0707d152a03f3c841
Richard L. Sweat Jr <Antimarvin@gmail.com>
2022-12-09 15:10:19 -0500

    builders Reimplemented

--- src/Managers/buildingManager.js
+++ src/Managers/buildingManager.js
@@ -3,19 +3,49 @@ let creepLogic = require('.././creeps/index');
 function buildingManager(castleRoom){
 
     Memory.buildingManager = {
-        creeps: [creepLogic.builder.defaultSettings(1)]
+        creeps: [creepLogic.builder.defaultSettings(castleRoom.name,1),
+                 creepLogic.builder.defaultSettings(castleRoom.name,2)]
     }
-    let constructionSites = [Game.constructionSites[i]]
-    let highPriorityRepair = _.filter(Game.structures, s => s.hits < s.hitsMax
+
+    let constructionSites = Object.keys(Game.constructionSites).map( function (key){
+        return {[key]: Game.constructionSites[key]}
+    })
+
+    let mystructures = []
+    for(let roomKey in Game.rooms){
+        let room = Game.rooms[roomKey]
+        //console.log(room.find(FIND_STRUCTURES, {filter: s => s.my}))
+        let myRoomStructures = room.find(FIND_STRUCTURES, {filter: s => s.my})
+        for(let structure of myRoomStructures){
+            mystructures.push(structure)
+        }
+    }
+
+    let highPriorityRepair = _.filter(mystructures, s => s.hits < s.hitsMax
         && (s.structureType === STRUCTURE_SPAWN
         || s.structureType === STRUCTURE_STORAGE
         || s.structureType === STRUCTURE_EXTENSION))
-    let mediumPriorityRepair = _.filter(Game.structures, s => s.hits < s.hitsMax
+    //console.log(JSON.stringify(highPriorityRepair))
+    let highPriorityRepairList = Object.keys(highPriorityRepair).map( function (key){
+        return {[key]: highPriorityRepair[key]}
+    })
+
+    //console.log(JSON.stringify(highPriorityRepairList))
+    let mediumPriorityRepair = _.filter(mystructures, s => s.hits < s.hitsMax
         && (s.structureType === STRUCTURE_LAB
             || s.structureType === STRUCTURE_CONTAINER
             || s.structureType === STRUCTURE_LINK))
-    let lowPriorityRepair = _.filter(Game.structures, s => s.hits < s.hitsMax
-        && (s.structureType === STRUCTURE_ROAD))
+    let mediumPriorityRepairList = Object.keys(mediumPriorityRepair).map( function (key){
+        return {[key]: mediumPriorityRepair[key]}
+    })
+
+    let lowPriorityRepair = _.filter(mystructures, s => s.hits < s.hitsMax
+        && (s.structureType === STRUCTURE_LAB
+            || s.structureType === STRUCTURE_CONTAINER
+            || s.structureType === STRUCTURE_LINK))
+    let lowPriorityRepairList = Object.keys(lowPriorityRepair).map( function (key){
+        return {[key]: lowPriorityRepair[key]}
+    })
 
     // Check to make sure all your creeps are spawned, if not add them to build queue
     for(let i = 0; i < Memory.buildingManager.creeps.length; i++) {
@@ -25,10 +55,42 @@ function buildingManager(castleRoom){
         }
     }
     let builders = _.filter(Game.creeps, c => c.memory.role === 'builder')
-    if(builders){
-        for(let builder in builders){
+    //console.log(JSON.stringify(builders))
+    if(Object.keys(builders).length > 0){
+        for(let b in builders){
+            let builder = builders[b]
+            console.log(`I have builder ${builder.name} currently doing ${builder.memory.job}`)
             if(builder.memory.job === false){
+                console.log(`The length of constructionsites is ${constructionSites.length}`)
+                console.log(`The length of high repair is ${highPriorityRepairList.length}`)
+                console.log(`The length of medium repair is ${mediumPriorityRepairList.length}`)
+                console.log(`The length of low repair is ${mediumPriorityRepairList.length}`)
+                if(constructionSites.length > 0){
+                    //console.log(JSON.stringify(constructionSite))
+                    let target = constructionSites.pop()
+                    builder.memory.job = "Build"
+                    builder.memory.jobTarget = target[Object.keys(target)[0]]
+                }
+                else if (highPriorityRepairList.length > 0){
+                    let target = highPriorityRepairList.pop()
+                    builder.memory.job = "Repair"
+                    builder.memory.jobTarget = target[Object.keys(target)[0]]
+                }
+                else if (mediumPriorityRepairList.length > 0){
+                    let target = mediumPriorityRepairList.pop()
 
+                    builder.memory.job = "Repair"
+                    builder.memory.jobTarget = target[Object.keys(target)[0]]
+                }
+                else if (lowPriorityRepairList.length > 0){
+                    let target = lowPriorityRepairList.pop()
+                    builder.memory.job = "Repair"
+                    builder.memory.jobTarget = target[Object.keys(target)[0]]
+                }
+                else {
+                    builder.memory.job = "Upgrade"
+                    builder.memory.jobTarget = builder.room.controller
+                }
             }
         }
     }
--- src/creeps/builder.js
+++ src/creeps/builder.js
@@ -1,4 +1,3 @@
-const upgrader = require("./upgrader");
 var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -11,43 +10,68 @@ var roleBuilder = {
         }
 
 
-        if(!creep.memory.working) {
+        if(!creep.memory.working || !creep.memory.job) {
 
-            let availableContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: s => (s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_STORAGE)
-                    && s.store.energy >= creep.store.getFreeCapacity()
-            })
+            if(creep.room.name === creep.memory.home){
+                let availableContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => (s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_STORAGE)
+                        && s.store.energy >= creep.store.getFreeCapacity()
+                })
 
-            if (availableContainer) {
-                if (creep.withdraw(availableContainer, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(availableContainer);
+                if (availableContainer) {
+                    if (creep.withdraw(availableContainer, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(availableContainer);
+                    }
                 }
+            }
+            else {
+                creep.moveTo(new RoomPosition(25,25,creep.memory.home))
             }
         }
         else if (creep.memory.working) {
 
-            let structure = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES)
-
-            if(structure){
-                if(creep.build(structure) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structure);
+            let target = new RoomPosition(creep.memory.jobTarget.pos.x,creep.memory.jobTarget.pos.y,creep.memory.jobTarget.pos.roomName)
+            console.log(`${creep.name} is in ${creep.room.name} and headed to ${target.roomName}`)
+            if (creep.room.name === target.roomName) {
+                console.log(`${creep.name} is in the right room`)
+                if (creep.pos.isNearTo(target)) {
+                    console.log(`${creep.name} is near its build target`)
+                    if (creep.memory.job === "Build") {
+                        if(creep.build(Game.getObjectById(creep.memory.jobTarget.id)) === ERR_INVALID_TARGET){
+                            creep.memory.job = false
+                            creep.memory.jobTarget = false
+                        }
+                    } else if (creep.memory.job === "Repair") {
+                        creep.repair(Game.getObjectById(creep.memory.jobTarget.id))
+                    } else if (creep.memory.job === "Upgrade") {
+                        creep.upgradeController(Game.getObjectById(creep.memory.jobTarget.id))
+                    } else {
+                        creep.say("Stalled")
+                    }
+                }
+                else{
+                    console.log(`${creep.name} is moving to it target`)
+                    creep.moveTo(target)
                 }
             }
             else {
-                upgrader.run(creep)
+                console.log(`${creep.name} is in the right room`)
+                creep.moveTo(new RoomPosition(target.x, target.y, target.roomName))
             }
         }
     },
-    defaultSettings: function (serialNumber){
+    defaultSettings: function (home, serialNumber){
+        let name = `Builder_${serialNumber}`
         return {
-            name: `Truck_${serialNumber}`,
+            name: name,
             baseBody: [],
-            scalingBody: [CARRY, CARRY, MOVE],
+            scalingBody: [WORK, CARRY, MOVE],
             memory: {
-                role: 'truck',
-                job : undefined,
-                jobTarget: undefined,
-                working: false
+                role: 'builder',
+                job : false,
+                jobTarget: false,
+                working: false,
+                home: home
             },
         }
     }
